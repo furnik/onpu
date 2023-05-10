c71974cc235231cb8248f93cb7d912babf215387
Serhii Furnik <serhii.furnik@moonion.com>
2023-05-10 22:41:12 +0300

    Updates

---
+++ static/js/379.aa79c2d9.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkweb=self.webpackChunkweb||[]).push([[379],{5379:function(e,n,a){a.r(n);a(2791);var l=a(568),o=a(1558),r=a(1348),s=a(184);n.default=function(e){var n=e.onClose,a=e.onChange,u=e.onSubmit;return(0,s.jsxs)(o.l,{onClose:n,children:[(0,s.jsx)(l.I,{placeholder:"en",onChange:a("en")}),(0,s.jsx)(l.I,{placeholder:"ua",onChange:a("ua")}),(0,s.jsx)(r.z,{onClick:u,variant:"primary",children:"Submit"})]})}}}]);
+//# sourceMappingURL=379.aa79c2d9.chunk.js.map
